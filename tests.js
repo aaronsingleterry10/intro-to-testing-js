e8a7891e3a09dbdf75a9f8e22847fdc30bf18f66
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-04-24 23:02:59 -0500

    added more expects

--- tests.js
+++ tests.js
@@ -66,4 +66,18 @@ describe('isEven', function() {
     it('should be defined a boolean', function() {
         expect(typeof isEven()).toBe('boolean');
     });
+    it('returns true when executed with 2', function() {
+        expect(isEven(2)).toBe(true);
+    });
+    it('returns true when executed with -4', function() {
+        expect(isEven(-4)).toBe(true);
+    });
+    it('returns false when executed with 3', function() {
+        expect(isEven(3)).toBe(false);
+    });
+    it('returns false when executed "banana"', function() {
+        expect(isEven('banana')).toBe(false);
+    });
+
+
 });
